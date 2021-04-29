1437b91f71dae32432faac0433b285cdc8bb0e08
Vitaliy-1915 <74369854+Vitaliy-1915@users.noreply.github.com>
2021-04-29 23:17:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+const Handlebars = require("handlebars");
+// const template = Handlebars.compile("Name: {{name}}");
+// console.log(template({ name: "Nils" }));
+
+import * as ALL from "./js/theme";
+
 import "./sass/main.scss";
