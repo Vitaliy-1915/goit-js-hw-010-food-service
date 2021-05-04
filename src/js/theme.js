1efbfb342055f1cacf82395b52d460237bd5819f
Vitaliy-1915 <74369854+Vitaliy-1915@users.noreply.github.com>
2021-05-04 20:40:51 +0300

    Update theme.js

--- src/js/theme.js
+++ src/js/theme.js
@@ -3,34 +3,54 @@ const Theme = {
   DARK: 'dark-theme',
 };
    
+const bodyClass = document.body;
+
+localStorage.setItem('DARK', Theme.DARK);
+const readDarkTheme = localStorage.getItem('DARK');
+localStorage.setItem('LIGHT', Theme.LIGHT);
+const readLightTheme = localStorage.getItem('LIGHT');
+
+    bodyClass.classList.add(localStorage.getItem('Theme') === null
+    ? Theme.LIGHT
+    : readDarkTheme,
+);
+
+console.log(localStorage.getItem('Theme'));
+console.log(Theme.LIGHT);
+console.log(readDarkTheme);
+
     const onCheckboxChangeClassBody = document.querySelector('#theme-switch-toggle');
     onCheckboxChangeClassBody.addEventListener('change', changeClassBody);  
 
 function changeClassBody(event) {
-    const bodyClass = document.body;
     
-        localStorage.setItem('Theme.DARK', Theme.DARK);
-        const readDarkTheme = localStorage.getItem('Theme.DARK');
+    
+        localStorage.setItem('DARK', Theme.DARK);
+        const readDarkTheme = localStorage.getItem('DARK');
         
-        localStorage.setItem('Theme.LIGHT', Theme.LIGHT);
-        const readLightTheme = localStorage.getItem('Theme.LIGHT');
+        localStorage.setItem('LIGHT', Theme.LIGHT);
+        const readLightTheme = localStorage.getItem('LIGHT');
     
     if (onCheckboxChangeClassBody.checked) {
             bodyClass.classList.add(readDarkTheme);
             bodyClass.classList.remove(readLightTheme);
 
-           localStorage.removeItem('Theme.LIGHT');
+           localStorage.removeItem('LIGHT');
            
         } else if (!onCheckboxChangeClassBody.checked) {
             bodyClass.classList.add(readLightTheme);
             bodyClass.classList.remove(readDarkTheme);
 
-            localStorage.removeItem('Theme.DARK');
-       };           
+            localStorage.removeItem('DARK');
+    };
+    
+
 };
 
 
-    
+
+
+
 
 
     
